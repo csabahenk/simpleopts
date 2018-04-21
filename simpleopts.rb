ba08f84467da9b07e1b6a10a97901bb35ae482c9
Csaba Henk <csaba@lowlife.hu>
2018-04-21 08:49:33 +0200

    fix formatting

--- simpleopts.rb
+++ simpleopts.rb
@@ -1,9 +1,6 @@
 require 'optparse'
 
 
-######################################################################################
-
-
 module SimpleOpts
 
   def self.represent v
@@ -29,49 +26,49 @@ module SimpleOpts
     fixer = proc { |c| (Class === c and c.name == "Fixnum") ? Integer : c }
     shortopts = []
     OptionParser.new { |op|
-       opts.each { |o,w|
-         # Mangling opts to OptionParser options in a disgraced manner
-         # - opt_name: <scalar> becomes:
-         #   op.on("-o", "--opt-name", fixer[<scalar>.class], <scalar>.to_s) {...}
-         # - opt_name: <class> becomes: op.on("-o", "--opt-name", fixer[<class>]) {...}
-         #   where fixer is needed to map arbitrary classes into the class set
-         #   accepted by OptionParser
-         defval = w[:default]
-         optclass = fixer.call case defval
-         when Class
-           defval
-         when nil
-           String
-         else
-           defval.class
-         end
-         # Classes don't match themselves with === operator,
-         # so the case construct has to dispatch on their names.
-         # Also some of the classes we are to dispatch on may
-         # not be defined (Date*) -- thus dispatching on the name
-         # avoids reference to an undefined.
-         valrep = case optclass.name
-         when 'Integer','Float'
-           "N"
-         when 'TrueClass','FalseClass'
-           "[BOOL]"
-         when 'Array'
-           "VAL,.."
-         when 'Regexp'
-           "PAT"
-         when 'Date','Time','DateTime'
-           "T"
-         else
-           "VAL"
-         end
-         shortie = "-#{o[0]}"
-         optargs = [
+      opts.each { |o,w|
+        # Mangling opts to OptionParser options in a disgraced manner
+        # - opt_name: <scalar> becomes:
+        #   op.on("-o", "--opt-name", fixer[<scalar>.class], <scalar>.to_s) {...}
+        # - opt_name: <class> becomes: op.on("-o", "--opt-name", fixer[<class>]) {...}
+        #   where fixer is needed to map arbitrary classes into the class set
+        #   accepted by OptionParser
+        defval = w[:default]
+        optclass = fixer.call case defval
+        when Class
+          defval
+        when nil
+          String
+        else
+          defval.class
+        end
+        # Classes don't match themselves with === operator,
+        # so the case construct has to dispatch on their names.
+        # Also some of the classes we are to dispatch on may
+        # not be defined (Date*) -- thus dispatching on the name
+        # avoids reference to an undefined.
+        valrep = case optclass.name
+        when 'Integer','Float'
+          "N"
+        when 'TrueClass','FalseClass'
+          "[BOOL]"
+        when 'Array'
+          "VAL,.."
+        when 'Regexp'
+          "PAT"
+        when 'Date','Time','DateTime'
+          "T"
+        else
+          "VAL"
+        end
+        shortie = "-#{o[0]}"
+        optargs = [
           shortopts.include?(shortie) ? [] : (shortopts << shortie; [shortie]),
           "--#{o.to_s.gsub "_", "-"}=#{valrep}",
           [optclass] + (Class === defval ? [] : [represent(defval)])
-         ].flatten
-         op.on(*optargs) { |v| opts[o][:cmdline] = v }
-       }
+        ].flatten
+        op.on(*optargs) { |v| opts[o][:cmdline] = v }
+      }
     }.parse! argv
     if config_file_opt
       config_file = (opts[config_file_opt]||{}).values_at(
